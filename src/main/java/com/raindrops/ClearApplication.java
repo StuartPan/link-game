222f122edc2031a802527ccb13cfc2f0794b7992
Raindrops <970380758@qq.com>
2025-01-21 23:47:57 +0800

    remove useless code

--- src/main/java/com/raindrops/ClearApplication.java
+++ src/main/java/com/raindrops/ClearApplication.java
@@ -1,15 +1,3 @@
-/**
- * Copyright (C) 2023, 南京晓鸣信息技术有限公司
- * This file is the private property of the copyright owner. It must not be copied,
- * distributed or used without the copyright owner's authorization.
- * Any unauthorized use, disclosure or distribution is strictly prohibited and may be unlawful.
- * Author: 南京晓鸣信息技术有限公司
- * Version: 1.0
- * License：Proprietary License
- * Created: 2025/1/20
- * Modified: 2025/1/20
- * Contact: 南京晓鸣信息技术有限公司
- */
 package com.raindrops;
 
 import com.almasb.fxgl.app.GameApplication;
@@ -352,6 +340,5 @@ public class ClearApplication extends GameApplication {
             entity.setProperty("col", position[1]);
             entity.setProperty("row", position[0]);
         }
-        System.out.println(FXGL.getGameWorld().getEntities());
     }
 }
